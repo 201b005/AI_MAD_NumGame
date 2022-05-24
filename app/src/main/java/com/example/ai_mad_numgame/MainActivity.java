f1570ac91372a18f982a48f910252a3c77068cad
imani03 <201b045@juetguna.in>
2022-05-24 12:51:27 +0530

    NumGame

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -72,7 +72,34 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
+        int operand2= random.nextInt(10);
+        correctButton = random.nextInt(4);
+        int correctAnswer = -100;
+        if(operators.equals("+"))
+        {
+            correctAnswer = operand1+operand2;
+        }
+        else if(operators.equals("-")){
+            correctAnswer = operand1 - operand2;
+        }
+        else if(operators.equals("*")){
+            correctAnswer = operand1 * operand2;
+        }
+        else{
+            correctAnswer = operand1 / operand2;
+        }
+        if(correctButton == 0){
+            button1.setText(correctAnswer + "");
+            button2.setText(correctAnswer + 1 + "");
+            button3.setText(correctAnswer + 2 + "");
+            button4.setText(correctAnswer + 3 + "");
+        }
+        if(correctButton == 1){
+            button1.setText(correctAnswer + "");
+            button2.setText(correctAnswer + 1 + "");
+            button3.setText(correctAnswer + 2 + "");
+            button4.setText(correctAnswer + 3 + "");
+        }
         //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
